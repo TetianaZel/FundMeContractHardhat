9276bd9e6ce8d0582a23121b5a3ffc1faad1f5c4
TetianaZel <tanyazzel96@gmail.com>
2022-12-08 20:03:55 +0000

    finished multisig wallet

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -25,6 +25,21 @@ contract MultiSigWallet {
         _;
     }
 
+    modifier txExists(uint _txId) {
+        require(_txId < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notApproved(uint _txId) {
+        require(!approved[_txId][msg.sender], "tx already approved");
+        _;
+    }
+
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
     Transaction[] public transactions;
     mapping(uint256 => mapping(address => bool)) public approved;
 
@@ -58,5 +73,45 @@ contract MultiSigWallet {
             Transaction({to: _to, value: _value, data: _data, executed: false})
         );
         emit Submit(transactions.length - 1);
+    }
+
+    function approve(
+        uint _txId
+    ) external onlyOwner txExists(_txId) notApproved(_txId) notExecuted(_txId) {
+        approved[_txId][msg.sender] = true;
+        emit Approve(msg.sender, _txId);
+    }
+
+    function _getApprovalsCount(uint _txId) private view returns (uint count) {
+        for (uint i; i < owners.length; i++) {
+            if (approved[_txId][owners[i]]) {
+                count += 1;
+            }
+        }
+    }
+
+    function execute(uint _txId) external txExists(_txId) notExecuted(_txId) {
+        require(
+            _getApprovalsCount(_txId) >= requiredApprovals,
+            "not enough approvals to execute"
+        );
+        Transaction storage transaction = transactions[_txId];
+        transaction.executed = true;
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        require(success, "tx failed");
+        emit Execute(_txId);
+    }
+
+    function revoke(
+        uint _txId
+    ) external onlyOwner notExecuted(_txId) txExists(_txId) {
+        require(
+            approved[_txId][msg.sender],
+            "tx was not approved by this owner"
+        );
+        approved[_txId][msg.sender] = false;
+        emit Revoke(msg.sender, _txId);
     }
 }
